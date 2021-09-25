20dde12fdd52b4c16405ee6d3430132159cd7f7e
ASUS <ASUS@Devdutt>
2021-09-25 12:09:07 +0530

    commit 1

---
+++ src/com/nt/test/Addition1.java
@@ -0,0 +1,14 @@
+package com.nt.test;
+
+public class Addition1 {
+
+	public static void main(String[] args) {
+
+		sum(10, 20);
+	}
+
+	public static void sum(int a, int b) {
+		System.out.println("Addition:- " + (a + b));
+	}
+
+}
